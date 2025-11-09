464c7bf6f2caa3fc22f6a3f54ce2fddffac5ee4e
prabalpk <prabalpk12@gmail.com>
2025-11-09 06:22:17 +0530

    Add initial project files and basic Python scripts
    Added PyCharm project configuration files and two Python scripts: 'basic_mathematical_operations.py' for performing arithmetic operations, and 'greeting.py' for greeting the user by name.

---
+++ basic_mathematical_operations.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+
+print ("addition: ", add)
+print ("subtraction: ", sub)
+print ("multiplication: ", mul)
+print ("division: ", div)
